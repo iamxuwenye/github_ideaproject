c65ac8f2b6d14e296caffd91d903b66bcfe1718d
xuwenye <1149393408@qq.com>
2022-10-20 09:45:38 +0800

    合并后

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,5 +1,8 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("master 第一次修改");
+        System.out.println("worker1 第一次修改");
+        System.out.println("master 第二次修改");
+        System.out.println("worker1 第一次修改");
     }
 }
